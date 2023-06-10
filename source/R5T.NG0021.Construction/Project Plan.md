cfc5cf9d67767f16641672dda1f577987e125555
davidcoats <david@getsafetycone.com>
2023-06-10 08:59:24 -0700

    Initial commit.

---
+++ source/R5T.NG0021.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.NG0021.Construction
+Construction console project for the R5T.NG0021 library.
---
+++ source/R5T.NG0021/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.NG0021
+{
+	/// <summary>
+	/// Microsoft.Build.Locator NuGet package selector.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
